cc06be000d87aca1d357f10fa2f891f7586f3df7
GibranSaputra11 <gibranbantul@gmail.com>
2025-03-20 10:54:07 +0700

    add prosedur insertionshort

--- InsertionShort.cpp
+++ InsertionShort.cpp
@@ -31,6 +31,26 @@ void input()
     }
 }
 
+void insertiosnShort()
+{ // Membuat prosedur insertionShort
+    int temp; // Membuat variable data temporer atau penyimpanan sementara
+    int j, i; // Membuat variable j sebagai penanda
+
+    for (i = 1; i <= n - 1; i++) // Step 1 
+    {
+        temp = arr[i]; // Step 2
+        j = i - 1; // Step 3
+        
+        while (j >= 0 && arr[j]) // Step 4
+        {
+            arr[j + 1] = arr[j]; // Step 4a
+            j--; // Step 4b
+        }
+
+        arr[j +1] = temp; // Step 5
+    }
+}
+
 int main()
 {
 }
